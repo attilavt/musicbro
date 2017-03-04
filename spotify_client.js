878cd13f10b83a45342963367329a10ff71bf4d8
attilavt <attilavt@gmail.com>
2017-03-04 12:40:13 +0100

    added request chaining, comments

--- spotify_client.js
+++ spotify_client.js
@@ -22,6 +22,11 @@ var sendRequest = function(path, method, callback) {
 
 };
 
+/**
+ * @param name Name of the artist
+ * @param deepCallback (optional) function to carry out with the result of this method after the method
+ * @return NULL. if you want to use the result, use it by providing a deepCallback
+ */
 var getArtistId =function (name, deepCallback) {
     var leUrl =  "search?q=" + name + "&type=artist&market=" + market + "&limit=1";
 
@@ -42,30 +47,46 @@ var getArtistId =function (name, deepCallback) {
     return sendRequest(leUrl, GET, callback);
 };
 
+/**
+ * @param name Name of the artist
+ * @param artistSpotifyId (optional) spotify ID of the artist. if not given, getArtistId method is used before sending the request for this method
+ * @param limit amount of similar artists wanted
+ * @param deepCallback (optional) function to carry out with the result of this method after the method
+ * @return NULL. if you want to use the result, use it by providing a deepCallback
+ */
 var getSimilarArtists = function (name, artistSpotifyId, limit, deepCallback) {
 
-    var leUrl =  "artists/" + artistSpotifyId + "/related-artists?country=" + market;
+    var doIt = function (spotifyId) {
+        var leUrl =  "artists/" + spotifyId + "/related-artists?country=" + market;
 
-    var callback = function(chunk) {
-        if (chunk.artists.length === 0 ){
-            console.log("No spotify similar artist found for artist with name"+ name);
-            return;
-        }
+        var callback = function(chunk) {
+            if (chunk.artists.length === 0 ){
+                console.log("No spotify similar artist found for artist with name"+ name);
+                return;
+            }
 
-        var result = [];
+            var result = [];
 
-        for(var i = 0; i < chunk.artists.length && i < limit; i++) {
-            var artist = chunk.artists[i];
-            result.push(artist.name);
-        }
+            for(var i = 0; i < chunk.artists.length && i < limit; i++) {
+                var artist = chunk.artists[i];
+                result.push(artist.name);
+            }
 
-        console.log("Found spotify similar artists ", result, "for artist with name "+ name);
-        if(deepCallback)
-            deepCallback(result);
-        return result;
+            console.log("Found spotify similar artists ", result, "for artist with name "+ name);
+            if(deepCallback)
+                deepCallback(result);
+            return result;
+        };
+
+        return sendRequest(leUrl, GET, callback);
     };
 
-    return sendRequest(leUrl, GET, callback);
+    if(!artistSpotifyId) {
+        getArtistId(name, doIt);
+    } else {
+        doIt(artistSpotifyId);
+    }
+
 };
 
 module.exports = {
