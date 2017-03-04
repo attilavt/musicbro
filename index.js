6be351a24a9e5f09bd781608b33798125b99fbf0
cookandcodeorg <news@cookandcode.org>
2017-03-04 20:07:43 +0100

    testvideo

--- index.js
+++ index.js
@@ -149,6 +149,13 @@ function receivedMessage(event) {
                     sendGenericMessage(senderID);
                     break;
 
+                    case 'video':
+                    testSendVideo(senderID);
+                    break;
+
+                    
+
+
                   default:
                     sendTextMessage(senderID, messageText);
                 }
@@ -171,6 +178,26 @@ function sendTextMessage(recipientId, messageText) {
 
 
 
+
+
+
+function testSendVideo(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+   message:{
+    attachment:{
+      type:"video",
+      payload:{
+        url:"https://www.youtube.com/watch?v=3NPxqXMZq7o"
+      }
+    }
+  }
+  };  
+
+  callSendAPI(messageData);
+}
 
 
 function sendGenericMessage(recipientId) {
@@ -245,7 +272,6 @@ function receivedPostback(event) {
 
 
 
-
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
@@ -268,5 +294,28 @@ function callSendAPI(messageData) {
   });  
 }
 
+
+
+function testLikeMusic(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 
